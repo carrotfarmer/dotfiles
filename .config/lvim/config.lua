42a351d570e2d86ee67fcf736d4a2a33226c7f18
Dhruva Srinivas <66675022+carrotfarmer@users.noreply.github.com>
2023-02-28 16:38:58 +0530

    feat: minimap

--- .config/lvim/config.lua
+++ .config/lvim/config.lua
@@ -143,14 +143,6 @@ lvim.plugins = {
   { "dag/vim-fish" },
   { "ekalinin/dockerfile.vim" },
   { "wakatime/vim-wakatime" },
-  {
-    "wfxr/minimap.vim",
-    config = function()
-      vim.cmd("let g:minimap_width = 10")
-      vim.cmd("let g:minimap_auto_start = 1")
-      vim.cmd("let g:minimap_auto_start_win_enter = 1")
-    end
-  },
   { "mattn/emmet-vim" },
   { "iamcco/coc-tailwindcss" },
   { "pantharshit00/vim-prisma" },
@@ -166,5 +158,61 @@ lvim.plugins = {
         -- refer to the configuration section below
       }
     end
+  },
+  {
+    "echasnovski/mini.map",
+    branch = "stable",
+    config = function()
+      require('mini.map').setup()
+      local map = require('mini.map')
+      map.setup({
+        integrations = {
+          map.gen_integration.builtin_search(),
+          map.gen_integration.diagnostic({
+            error = 'DiagnosticFloatingError',
+            warn  = 'DiagnosticFloatingWarn',
+            info  = 'DiagnosticFloatingInfo',
+            hint  = 'DiagnosticFloatingHint',
+          }),
+        },
+        symbols = {
+          encode = map.gen_encode_symbols.dot('4x2'),
+        },
+        window = {
+          side = 'right',
+          width = 20, -- set to 1 for a pure scrollbar :)
+          winblend = 15,
+          show_integration_count = false,
+        },
+      })
+    end
+  },
+}
+
+lvim.autocommands = {
+  {
+    { "BufEnter", "Filetype" },
+    {
+      desc = "Open mini.map and exclude some filetypes",
+      pattern = { "*" },
+      callback = function()
+        local exclude_ft = {
+          "qf",
+          "NvimTree",
+          "toggleterm",
+          "TelescopePrompt",
+          "alpha",
+          "netrw",
+        }
+
+        local map = require('mini.map')
+        if vim.tbl_contains(exclude_ft, vim.o.filetype) then
+          vim.b.minimap_disable = true
+          map.close()
+        elseif vim.o.buftype == "" then
+          map.open()
+        end
+      end,
+    },
   },
 }
